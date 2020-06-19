fc0a3cca383aa4a0182ab88ab0b48cc208ad7a45
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-06-18 23:18:11 -0600

    added header

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div classname="header">
+            <h1>MadLibs</h1>
+            <p>Fill out the fields and click the generate btn to see your MadLibs story</p>
+        </div>
+    )
+}
+
+export default Header;
