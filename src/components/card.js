f8f56bf4e98b8612cd8df2f60533c0bbfc8d465a
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-06-22 21:07:41 -0600

    fafed about

--- src/components/card.js
+++ src/components/card.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 import Input from './input';
+import { timers } from 'jquery';
 
 
 class card extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            color:'BLUE',
+            pluralNoun: ""
+        }
+    }
+
+    handleInputChange() {
+        this.setState({color:'red'})
+    }
+
     render() {
         return (
             <div className="card">
+                <h1>{this.state.color}</h1>
+                <input/>
+            </div>
+        );
+        return (
+            <div className="card">
+                <h1>{this.state.color}</h1>
                 { Input('color') }
                 { Input('plual noun') }
             </div>
-        )
+        );
     }
 }
 
